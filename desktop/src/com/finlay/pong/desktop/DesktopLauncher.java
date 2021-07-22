e6adc20540676fccb71dc3114ccc3610ac8c62be
Finlay-was-taken <FinlayBarford@outlook.com>
2021-07-22 18:11:38 +0100

    A test i suppose

--- desktop/src/com/finlay/pong/desktop/DesktopLauncher.java
+++ desktop/src/com/finlay/pong/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.finlay.pong.PongGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "oi oi";
+		config.width = 700;
+		config.height = 700;
 		new LwjglApplication(new PongGame(), config);
 	}
 }
