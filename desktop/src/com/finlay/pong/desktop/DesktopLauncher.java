235b6941b0eb7478bf06504722d949d20e4f5040
DavidCrossman <davidocrossman@gmail.com>
2021-07-22 20:12:49 +0100

    Updated to LWJGL3

--- desktop/src/com/finlay/pong/desktop/DesktopLauncher.java
+++ desktop/src/com/finlay/pong/desktop/DesktopLauncher.java
@@ -1,24 +1,24 @@
 package com.finlay.pong.desktop;
 
 import com.badlogic.gdx.Files;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.finlay.pong.PongGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Pong";
-		config.vSyncEnabled = true;
-		config.foregroundFPS = 60;
-		config.backgroundFPS = 30;
-		config.fullscreen = false;
-		config.width = 700;
-		config.height = 700;
-		config.resizable = true;
-		config.samples = 4; // MSAA sample number
-		config.undecorated = false; // Could be used for windowed fullscreen
-		config.addIcon("textures/icon.png", Files.FileType.Internal);
-		new LwjglApplication(new PongGame(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Pong");
+		config.useVsync(true);
+		config.setForegroundFPS(60);
+		config.setIdleFPS(60);
+		config.setWindowedMode(700, 700);
+		config.setWindowSizeLimits(300, 300, -1, -1);
+		config.setResizable(true);
+		config.useOpenGL3(true, 3, 3);
+		config.setDecorated(true); // Could be used for windowed fullscreen
+		config.setWindowIcon(Files.FileType.Internal, "textures/icon.png");
+		new Lwjgl3Application(new PongGame(), config);
 	}
 }
